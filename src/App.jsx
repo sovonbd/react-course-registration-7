dfbe3f7834f964b145306af3832c64d370ec8d5b
sovonbd <sovonsdp@gmail.com>
2023-09-14 12:04:45 -0500

    intalled react and tailwaind for the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-3xl text-[#1C1B1B] font-bold text-center py-12">Course Registration</h3>
+    </>
+  );
+}
+
+export default App;
